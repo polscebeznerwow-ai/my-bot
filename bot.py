1e13070d322d9efa01fd5af4d17034748628d953
polscebeznerwow-ai <polscebeznerwow@gmail.com>
2026-03-30 17:42:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telegram.ext import (
 
 logging.basicConfig(level=logging.INFO)
 
-BOT_TOKEN = 8525815500:AAHOvGN8rglwtRr8HDzW4DhONyI78iLJZOk
+BOT_TOKEN = "8525815500:AAHOvGN8rglwtRr8HDzW4DhONyI78iLJZOk"
 ADMIN_ID   = 980725289
 
 USERS = {
